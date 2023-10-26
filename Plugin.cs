8e066fb4acce207fc6f181469ef3f839ef4878fb
Josiwe <jordan.weberflink@gmail.com>
2023-10-26 10:31:23 -0700

    Fixed issue with zooming in all the way; updated zoom speed to scale with additional zoom distance.

--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using Eremite.View.Cameras;
+using UnityEngine;
 
 namespace Josiwe.ATS.Zoom
 {
@@ -37,24 +38,31 @@ namespace Josiwe.ATS.Zoom
             ZoomConfig config = GetZoomConfig();
 
             int zoomMultiplier = 1;
-            if(config != null)
+            if (config != null)
             {
                 zoomMultiplier = config.ZoomLimitMultiplier;
             }
 
+            // zoomLimit is used as to define the min/max range for camera z
+            // we want to increase x (the outer zoom limit) but leave y untouched
             float x = -20f * zoomMultiplier;
-            float y = -8f * zoomMultiplier;
+            float y = -8f;
+            __instance.ZoomLimit = new Vector2(x, y);
 
-            __instance.ZoomLimit = new UnityEngine.Vector2(x, y);
+            // with more zoom space to cross, we need a speedier zoom
+            if(zoomMultiplier != 0)
+            {
+                __instance.zoomSmoothTime = __instance.zoomSmoothTime / zoomMultiplier;
+            }
         }
 
         [HarmonyPatch(typeof(MainController), nameof(MainController.OnServicesReady))]
         [HarmonyPostfix]
         private static void HookMainControllerSetup()
         { 
-            // This method will run after game load (Roughly on entering the main menu)
-            // At this point a lot of the game's data will be available.
-            // Your main entry point to access this data will be `Serviceable.Settings` or `MainController.Instance.Settings`
+            // __instance method will run after game load (Roughly on entering the main menu)
+            // At __instance point a lot of the game's data will be available.
+            // Your main entry point to access __instance data will be `Serviceable.Settings` or `MainController.Instance.Settings`
             Instance.Logger.LogInfo($"Performing game initialization on behalf of {PluginInfo.PLUGIN_GUID}.");
             Instance.Logger.LogInfo($"The game has loaded {MainController.Instance.Settings.effects.Length} effects.");
         }
@@ -64,9 +72,9 @@ namespace Josiwe.ATS.Zoom
         private static void HookEveryGameStart()
         {
             // Too difficult to predict when GameController will exist and I can hook observers to it
-            // So just use Harmony and save us all some time. This method will run after every game start
+            // So just use Harmony and save us all some time. __instance method will run after every game start
             var isNewGame = MB.GameSaveService.IsNewGame();
-            WriteLog($"Entered a game. Is this a new game: {isNewGame}.");
+            WriteLog($"Entered a game. Is __instance a new game: {isNewGame}.");
         }
 
         static ZoomConfig GetZoomConfig()
